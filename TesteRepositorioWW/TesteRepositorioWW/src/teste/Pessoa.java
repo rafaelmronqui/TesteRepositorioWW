112398c57a14f222ac27f1886d3e799517ea2066
rafaelmronqui <rafaelmronqui@gmail.com>
2022-06-28 15:44:14 -0300

    Terceiro commit

---
+++ TesteRepositorioWW/TesteRepositorioWW/src/teste/Pessoa.java
@@ -0,0 +1,23 @@
+package teste;
+
+public class Pessoa {
+	private String nome;
+	private int idade;
+
+	public final String getNome() {
+		return nome;
+	}
+
+	public final void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public final int getIdade() {
+		return idade;
+	}
+
+	public final void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+}
